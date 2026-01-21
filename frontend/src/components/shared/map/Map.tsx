8dd340024f033d73cde184f6b5f3b80d0b821442
swoocn <swooshinn@gmail.com>
2026-01-20 21:21:54 -0500

    Align logo splash mark location w notifications  popup on FE.

--- frontend/src/components/shared/map/Map.tsx
+++ frontend/src/components/shared/map/Map.tsx
@@ -360,12 +360,13 @@ const Map = ({
       )}
       {isSigningInUser ? (
         <div className="w-full flex-1 fixed bottom-0 h-[calc(100vh-76.19px)] left-0 right-0 bg-[#f5f1e6] ">
-          <div className="flex justify-center items-center w-full h-full">
+          <div className="flex justify-center items-start w-full h-full pt-[16vh] sm:pt-[18vh]">
             <Image 
               src="/default.png" 
-              width={120} 
-              height={140} 
-              alt="splashscreen" 
+              width={150} 
+              height={176}
+              alt="splashscreen"
+              priority
             />
           </div>
         </div>
